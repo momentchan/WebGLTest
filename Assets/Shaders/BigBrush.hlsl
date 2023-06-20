90e053760fab3f560e8207a9e52b63eb5a9acd6f
momentchan <mingjyunhung@gmail.com>
2023-06-20 19:22:27 +0800

    update brush

--- Assets/Shaders/BigBrush.hlsl
+++ Assets/Shaders/BigBrush.hlsl
@@ -1,7 +1,7 @@
 #include "Assets/Packages/unity-gist/Cginc/Shape.cginc"
 #include "Assets/Packages/unity-gist/Cginc/Random.cginc"
 #include "Assets/Packages/unity-gist/Cginc/SimplexNoise2D.cginc"
-#include "Assets/Packages/unity-gist/Cginc/PhotoshopMath.cginc"
+
 
 void DrawBigBrush_float(float2 UV, float2 Center, float Count, float Width, float Height, float ratio, float strength, out float Out)
 {
@@ -10,7 +10,7 @@ void DrawBigBrush_float(float2 UV, float2 Center, float Count, float Width, floa
     {
         float n = nrand(float2(i * 0.45, i * 1.89));
         
-        float offset = snoise(float2(n * 34.5, UV.y*2)) * 0.005;
+        float offset = 0;//snoise(float2(n * 34.5, UV.y*2)) * 0.05;
         
         float2 size = frac(float2(n * 43.4, n * 123.8));
         
@@ -27,7 +27,7 @@ void DrawBigBrush_float(float2 UV, float2 Center, float Count, float Width, floa
         float r = ratio * speed;
         float fade = 1 - smoothstep(delay + r, delay + r + 0.05, UV.y);
         
-        float o = drawEllipse(UV + offset, float2(x, y), w, h) * strength * fade;
+        float o = drawEllipse(UV + offset, float2(x, y), w, h) * strength* fade;
         Out += o;
         Out = saturate(Out);
         
@@ -35,4 +35,46 @@ void DrawBigBrush_float(float2 UV, float2 Center, float Count, float Width, floa
     }
     Out = saturate(Out) * 0.5;
     
+}
+
+
+float DrawSingle(float2 UV, float n, float Width, float Height, float ratio, float strength)
+{
+    float d = length(UV);
+        
+    float offset = snoise(float2(n * 123.45, UV.y * 4))*0.1;
+        
+    float2 size = frac(float2(n * 43.4, n * 123.8));
+        
+    float x = lerp(-1, 1, n);
+    //x = 0;
+    float y = lerp(-0.2, 0.2, frac(n * 133));
+    //y = 0;
+    float w = lerp(0.5, 1.5, size.x) * Width;
+   // float w =  Width;
+    float h = lerp(0.3, 1, size.y) * Height;
+    
+    float intensity = lerp(0.1, 1, frac(n * 345.5));
+    //float h = Height;
+        
+    float delay = -lerp(0, 0.2, frac(n * 32.5));
+    float speed = lerp(0.6, 1, frac(n * 69.3));
+        
+        
+    float r = ratio * speed;
+    float fade = 1 - smoothstep(delay + r, delay + r + 0.05, UV.y+0.5);
+        
+    return saturate(drawEllipse(UV + offset, float2(x, y), w, h)) * strength * intensity * fade;
+}
+
+void DrawBigBrushNew_float(float2 UV, float2 id, float Width, float Height, float ratio, float strength, out float Out)
+{
+    Out = 0;
+    for (int x = -10; x <= 10; x++)
+    {
+        float2 offs = float2(x, 0);
+        
+        float n = nrand(id + offs);
+        Out += DrawSingle(UV - offs, n, Width, Height, ratio, strength) / 21;
+    }
 }
